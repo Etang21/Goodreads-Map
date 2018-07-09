f2cf0f1c2b4c2cfefe229cc3c62025f1eb3639f2
Eric Tang <erictang1729@gmail.com>
2018-07-09 15:45:17 -0400

    Style login page

--- app/assets/javascripts/components/_login.js.jsx
+++ app/assets/javascripts/components/_login.js.jsx
@@ -1,9 +1,14 @@
 const Login = ({user_id, handleSubmit, handleChange}) => (
-  <form onSubmit={handleSubmit} onChange={handleChange}>
-    <label>
-      Goodreads ID:
-      <input type="text" value={user_id} />
-    </label>
-    <input type="submit" value="Go!" />
-  </form>
+  <div className="jumbotron">
+    <h1 className="display-4">Welcome to the Goodreads Map</h1>
+    <form  onSubmit={handleSubmit} onChange={handleChange}>
+      <div className="form-group">
+        <label>Goodreads ID</label>
+        <input type="text" className="form-control" value={user_id} />
+      </div>
+      <p className="lead">
+        <input className="btn btn-primary btn-lg" type="submit" value="Go!" />
+      </p>
+    </form>
+  </div>
 );
