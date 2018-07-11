3f4bef6f50cc00ac4af94d2646af4353743cd775
Eric Tang <erictang1729@gmail.com>
2018-07-11 18:27:56 -0400

    Pass client from controller to React

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,19 @@
 class HomeController < ApplicationController
 
-  # Here we authenticate the user to generate our Goodreads client
   def index
-    request_token = OAuth::Consumer.new(
-      "msEIA0FG34FG9peoBVH5g",
-      "a46rPARjz42Umqr36N2o0S72tdvPlKDlVFKD2FTYl8",
-      site: "https://www.goodreads.com"
-    ).get_request_token
-    if !params.key?("authorize")
-      # If no authorization, must authenticate
+    if !params.key?("authorize") # Try authorizing
+      request_token = OAuth::Consumer.new(
+        "msEIA0FG34FG9peoBVH5g",
+        "a46rPARjz42Umqr36N2o0S72tdvPlKDlVFKD2FTYl8",
+        site: "https://www.goodreads.com"
+      ).get_request_token
       current_url = request.original_url
       redirect_to(request_token.authorize_url + "&oauth_callback=#{current_url}")
+    elsif params["authorize"] == "1" # User authorized
+      @client = Goodreads.new(oauth_token: params["oauth_token"])
+    elsif params["authorize"] == "0" # User declined to authorize
+      Rails.logger.debug("User has declined to authenticate")
     end
-
   end
 
 end
