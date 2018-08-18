a1878c32bab9aae2133802633aba00517f9f5a44
Eric Tang <erictang1729@gmail.com>
2018-08-18 18:38:25 -0400

    Spin out OAuth consumer into helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+  helper_method :get_oauth_consumer
+
+  def get_oauth_consumer
+    return OAuth::Consumer.new(
+      "msEIA0FG34FG9peoBVH5g",
+      "a46rPARjz42Umqr36N2o0S72tdvPlKDlVFKD2FTYl8",
+      site: "https://www.goodreads.com"
+    )
+  end
+
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,27 +2,16 @@ class HomeController < ApplicationController
 
   def index
     if !(session[:token] && session[:token_secret] && params["authorize"]) # Try authorizing
-      request_token = OAuth::Consumer.new(
-        "msEIA0FG34FG9peoBVH5g",
-        "a46rPARjz42Umqr36N2o0S72tdvPlKDlVFKD2FTYl8",
-        site: "https://www.goodreads.com"
-      ).get_request_token
-      Rails.logger.debug(request_token.token)
-      Rails.logger.debug(request_token.secret)
+      consumer = get_oauth_consumer
+      request_token = consumer.get_request_token
       session[:token] = request_token.token
       session[:token_secret] = request_token.secret
       current_url = request.original_url
       redirect_to(request_token.authorize_url + "&oauth_callback=#{current_url}")
 
     elsif params["authorize"] == "1" # User authorized
-      Rails.logger.debug(session[:token])
-      Rails.logger.debug(session[:token_secret])
       request_hash = { oauth_token: session[:token], oauth_token_secret: session[:token_secret]}
-      consumer = OAuth::Consumer.new(
-        "msEIA0FG34FG9peoBVH5g",
-        "a46rPARjz42Umqr36N2o0S72tdvPlKDlVFKD2FTYl8",
-        site: "https://www.goodreads.com"
-      )
+      consumer = get_oauth_consumer
       request_token  = OAuth::RequestToken.from_hash(consumer, request_hash)
       access_token = request_token.get_access_token
       client = Goodreads.new(oauth_token: access_token)
@@ -32,6 +21,8 @@ class HomeController < ApplicationController
       Rails.logger.debug("User has declined to authenticate")
       @user_id = ""
     end
+    
     @authorize = params["authorize"]
   end
+
 end
