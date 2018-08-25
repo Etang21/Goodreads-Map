a39e976a3414f26960ced834bcacde905d9c18c9
Eric Tang <erictang1729@gmail.com>
2018-08-25 09:33:14 -0400

    Remove unused Books controller, views, tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   root 'home#index'
   get '/home', to: 'home#index'
 
-  resources :books # deprecated
-
   namespace :api do
     namespace :v1 do
       resources :authors, only: [:index] #Takes author_id param
