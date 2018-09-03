7c67bed988eef148a4e81d49995905c8ffdc7af4
Eric Tang <erictang1729@gmail.com>
2018-09-03 10:45:02 -0700

    Fix OAuth bug by caching access_token

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
 
   def index
+    #TODO: Do we have to cache the token and token_secret? Could replace with just caching access_token info.
     if !(session[:token] && session[:token_secret] && params["authorize"]) # Try authorizing
       consumer = get_oauth_consumer
       request_token = consumer.get_request_token
@@ -10,10 +11,19 @@ class HomeController < ApplicationController
       redirect_to(request_token.authorize_url + "&oauth_callback=#{current_url}")
 
     elsif params["authorize"] == "1" # User authorized
-      request_hash = { oauth_token: session[:token], oauth_token_secret: session[:token_secret]}
+      if !(session[:access_token] && session[:access_token_secret])
+        # No access_token, so generate access_token
+        request_hash = { oauth_token: session[:token], oauth_token_secret: session[:token_secret]}
+        consumer = get_oauth_consumer
+        request_token  = OAuth::RequestToken.from_hash(consumer, request_hash)
+        access_token = request_token.get_access_token
+        # in the initial session, we'll store token and secret somewhere
+        session[:access_token] = access_token.token
+        session[:access_token_secret] = access_token.secret
+      end
+      # in subsequent sessions, we'll rebuild the access token
       consumer = get_oauth_consumer
-      request_token  = OAuth::RequestToken.from_hash(consumer, request_hash)
-      access_token = request_token.get_access_token
+      access_token = OAuth::AccessToken.new(consumer, session[:access_token], session[:access_token_secret])
       client = Goodreads.new(oauth_token: access_token)
       @user_id = client.user_id
 
@@ -21,7 +31,7 @@ class HomeController < ApplicationController
       Rails.logger.debug("User has declined to authenticate")
       @user_id = ""
     end
-    
+
     @authorize = params["authorize"]
   end
 
