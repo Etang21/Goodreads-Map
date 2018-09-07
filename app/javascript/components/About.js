ea5896c075b5d465e3a90a99e2e5336fc5cbc9d7
Eric Tang <erictang1729@gmail.com>
2018-09-07 11:43:04 -0700

    Change class to className

--- app/javascript/components/About.js
+++ app/javascript/components/About.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div class="container">
-      <h3 class="my-4">About</h3>
+    <div className="container">
+      <h3 className="my-4">About</h3>
       <p>Novel Novels is a platform for Goodreads users to discover authors from underrepresented groups and understand the demographics of who they’re reading. With a focus on female and international authors, we hope that you’ll broaden your horizons and diversify your reading list.</p>
       <p>Keep an eye out for our recommendations, where we’ll generate personalized book recommendations based on your reading history.</p>
       <p>This site is powered by <a href="https://www.goodreads.com/">Goodreads</a>.</p>
@@ -13,7 +13,7 @@ const About = () => {
 ">Male Authors Are Four Times More Likely to Recommend Books by Men than by Women</a> - Bustle</p>
       <p><a href="https://ayearofreadingtheworld.com/what-on-earth-am-i-doing/">A Year of Reading the World</a> - Ann Morgan</p>
       <h5>Contact</h5>
-      <p>We would love to hear your feedback and ideas! Feel free to reach out at erictang1729 at gmail dot com.</p>
+      <p>We would love to hear your feedback and ideas! Feel free to reach out at erictang1729 @ gmail . com.</p>
     </div>
   )
 }
