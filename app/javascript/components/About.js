9f7c95b5db180c9aca793ba88bc256202b12afb2
Eric Tang <erictang1729@gmail.com>
2018-09-07 11:39:31 -0700

    First pass at populating About page

--- app/javascript/components/About.js
+++ app/javascript/components/About.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 const About = () => {
   return (
-    <h1>Information about Novel Novels</h1>
+    <div class="container">
+      <h3 class="my-4">About</h3>
+      <p>Novel Novels is a platform for Goodreads users to discover authors from underrepresented groups and understand the demographics of who they’re reading. With a focus on female and international authors, we hope that you’ll broaden your horizons and diversify your reading list.</p>
+      <p>Keep an eye out for our recommendations, where we’ll generate personalized book recommendations based on your reading history.</p>
+      <p>This site is powered by <a href="https://www.goodreads.com/">Goodreads</a>.</p>
+      <h5>Links</h5>
+      <p><a href="https://www.goodreads.com/blog/show/475-sex-and-reading-a-look-at-who-s-reading-whom">Who’s Reading Whom</a> - Goodreads</p>
+      <p><a href="https://www.bustle.com/p/a-breakdown-of-by-the-book-columns-shows-that-male-authors-are-four-times-more-likely-to-recommend-books-by-men-than-by-women-10244493
+">Male Authors Are Four Times More Likely to Recommend Books by Men than by Women</a> - Bustle</p>
+      <p><a href="https://ayearofreadingtheworld.com/what-on-earth-am-i-doing/">A Year of Reading the World</a> - Ann Morgan</p>
+      <h5>Contact</h5>
+      <p>We would love to hear your feedback and ideas! Feel free to reach out at erictang1729 at gmail dot com.</p>
+    </div>
   )
 }
 
