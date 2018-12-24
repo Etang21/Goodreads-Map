14b8aefc0bbce1e4b8682d3fb628575a2150e5fc
Eric Tang <erictang1729@gmail.com>
2018-12-24 11:30:23 -0800

    Initial populating database genres with potential recommendations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Here we seed with potential recommendations from existing lists
+require 'goodreads'
+
+Goodreads.configure(
+     api_key: "msEIA0FG34FG9peoBVH5g",
+     api_secret: "a46rPARjz42Umqr36N2o0S72tdvPlKDlVFKD2FTYl8"
+ )
+
+puts Genre.first
+
+client = Goodreads.new()
+
+shelf = client.shelf(1095613, 'india')
+shelf.books.each do |item|
+  puts item.book.id
+  book_info = client.book(item.book.id)
+  puts book_info.popular_shelves
+  book_info.popular_shelves.shelf.each do |s|
+    puts s
+    Genre.create(name: s.name)
+  end
+end
